16ad3440c80276e21c567ee695e2dbb598d460c2
Mojtaba Espari Pour <esparipour@gmail.com>
2023-10-02 07:46:45 +0330

    initial commit

---
+++ src/home.js
@@ -0,0 +1,9 @@
+console.log("Hello world!");
+console.log("I'm from JS");
+// console.warn("asdasd");
+
+setTimeout(function () {
+  console.log("hello from timeout.");
+}, 5000);
+
+console.log("salam");
